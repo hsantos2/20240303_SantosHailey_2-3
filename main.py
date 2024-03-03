223440adbfa8e72cbd9822a6ecaea3b4facec6fb
hsantos2 <159508866+hsantos2@users.noreply.github.com>
2024-03-03 21:11:47 +0000

    Assignment 2-3

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ wholesalePrice = 200.00
 
 # Write your assignment statements here for profit, salePrice, and saleProfit
 
+profit = retailPrice - wholesalePrice
+salePrice = retailPrice-(retailPrice*.25)
+saleProfit = salePrice - wholesalePrice
+
 print("Item Name: " + itemName)
 print("Retail Price: $" + str(retailPrice))
 print("Wholesale Price: $" + str(wholesalePrice))
